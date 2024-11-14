d4e81434bf44d05aa341858a3f4d32fac3b26972
DoNgocDong <ngocdong2110.2003@gmail.com>
2024-11-14 19:15:59 +0700

    create UI layout

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ const http = require("http");
 const config = require("./config/env.config");
 
 const app = express();
+const port = config.app.port;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
 const httpServer = http.createServer(app);
-const port = config.app.port;
 
 httpServer.listen(port, () => {
   console.log(`Application running on port ${port}`)
-})
+});
